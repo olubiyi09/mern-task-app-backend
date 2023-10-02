fff4e730efddf6226e1835434de647277b537d8a
olubiyi09 <olubiyi09@gmail.com>
2023-10-03 02:59:27 +0400

    complete project

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,16 @@ const connectDB = require("./config/connectDB")
 const mongoose = require("mongoose")
 const Task = require("./models/taskModel")
 const taskRoute = require("./routes/taskRoute")
+const cors = require("cors")
 
 const app = express()
 
 // Middlware
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"]
+}))
 app.use("/api/tasks", taskRoute)
 
 // Routes
@@ -24,7 +28,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000
 
 
-mongoose 
+mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
